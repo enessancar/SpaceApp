827088f5c24813b4aafb01dcd7a16107d9b5bdf9
enessancar <enes57751907@gmail.com>
2023-10-12 16:40:29 +0300

    I xreated launch service

--- SpaceX/Service/LaunchService.swift
+++ SpaceX/Service/LaunchService.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+protocol LaunchServiceProtocol {
+    func downloadLaunches(type: LaunchType, completion: @escaping([Launch]?) -> ())
+    func downloadLaunchDetail(id: String, completion: @escaping(Launch?) -> ())
+}
+
+final class LaunchService: LaunchServiceProtocol {
+    func downloadLaunches(type: LaunchType, completion: @escaping ([Launch]?) -> ()) {
+        var urlString = ""
+        
+        switch type {
+        case .upcoming:
+            urlString = APIURLs.upcomingLaunches
+        case .past:
+            urlString = APIURLs.pastLaunches
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        NetworkManager.shared.download(url: url) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let data):
+                completion(NetworkManager.shared.handleWithData(data, type: [Launch].self))
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+    
+    func downloadLaunchDetail(id: String, completion: @escaping (Launch?) -> ()) {
+        guard let url = URL(string: APIURLs.launchDetail(id: id)) else { return }
+        
+        NetworkManager.shared.download(url: url) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let data):
+                completion(NetworkManager.shared.handleWithData(data, type: Launch.self))
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+}
