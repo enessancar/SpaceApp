d063cbe61da0bfbe49692c019eedf241528ed0f6
enessancar <enes57751907@gmail.com>
2023-10-13 16:30:33 +0300

    I created link item view

--- SpaceX/Core/Components/Views/PastLaunchDateView.swift
+++ SpaceX/Core/Components/Views/PastLaunchDateView.swift
@@ -5,4 +5,47 @@
 //  Created by Enes Sancar on 13.10.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class PastLaunchDateView: UIView {
+    
+    private var prefixLabel: GFBodyLabel!
+    private var dateLabel: GFTitleLabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configurePrefixLabel()
+        configureDateLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func setup(launch: Launch) {
+        dateLabel.text = launch._launchDate
+    }
+    
+    private func configurePrefixLabel() {
+        prefixLabel = GFBodyLabel(text: "Launch Date:", textAlignment: .left, fontSize: .title2)
+        addSubview(prefixLabel)
+        
+        let width = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8PlusZoomed ? 76 : 100
+        prefixLabel.snp.makeConstraints { make in
+            make.leading.top.bottom.equalToSuperview()
+            make.width.equalTo(width)
+        }
+    }
+    
+    private func configureDateLabel() {
+        dateLabel = GFTitleLabel(text: "", textAlignment: .left, fontSize: .title2)
+        addSubview(dateLabel)
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+            make.leading.equalTo(prefixLabel.snp.trailing).offset(4)
+        }
+    }
+}
